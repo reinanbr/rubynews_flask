e6d4077b00a999465092b815c6dcc4c0a2ba4ed4
reinanbr <perseu912@gmail.com>
2023-04-10 17:48:59 -0300

    importing template with blueprint

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask
 from routes.index import index_page
 from routes.news import news_page
 
+
 app = Flask(__name__)
+
 
 app.register_blueprint(index_page)
 app.register_blueprint(news_page)
