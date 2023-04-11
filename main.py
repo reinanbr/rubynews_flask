cb192e76a9e9ac962b0ceaf5460d523b2c93f499
reinanbr <perseu912@gmail.com>
2023-04-10 22:02:56 -0300

    added upload image news with  title as url for img

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 from routes.index import index_page
 from routes.news import news_page
 
+UPLOAD_FOLDER = 'upload'
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#33434GTDFDFDFGGffsdfdsKijoQ8z\n\xec]/'
 
 app.register_blueprint(index_page)
 app.register_blueprint(news_page)
--- routes/news/news.py
+++ routes/news/news.py
@@ -1,6 +1,9 @@
-from flask import Blueprint,render_template,request,session, redirect,url_for,flash, send_from_directory
+from flask import Blueprint,render_template,current_app,request,session,jsonify, redirect,url_for,flash, send_from_directory
 from flask import jsonify
 from json import dump
+from werkzeug.utils import secure_filename
+
+
 
 from urllib.parse import quote
 from random import choice
@@ -9,14 +12,61 @@ import os
 import time
 
 
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+UPLOAD_FOLDER = dirname + "/upload"
+ALLOWED_EXTENSIONS = ["png", "jpeg", "jpg"]
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 # Defining a blueprint
 news_page = Blueprint(
     'news', __name__,
+    url_prefix='/news',
     template_folder='templates',
     static_folder='static',
     static_url_path='')
 
-@news_page.route('/news')
+@news_page.route('/add_news')
+def add_news():
+    return render_template('add_news.html')
+
+
+@news_page.route('/save_news',methods=['GET', 'POST'])
+def save_news():
+    if request.method=='POST':
+        news = (request.form.get('news'))
+        title = (request.form.get('title'))
+        
+        date = dt.datetime.now().strftime('%d/%m/%Y')
+        hour = dt.datetime.now().strftime('%h:%M')
+        
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            namefiext = tuple(file.filename.split('.'))
+            url_filename =  f'{quote(title)}.{ext}'
+            filename = secure_filename(url_filename)
+            print(UPLOAD_FOLDER,filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+
+    
+    
+    
+    return jsonify({"sucess":"ok"})
+
+
+@news_page.route('/')
 def index():
     userAgent = request.headers.get('User_Agent').lower()
     return render_template('news.html')
+
+
+
